7c77acdf34d79832cdfeafb24171fdb3b83c53ad
Rozo Leonel (CR/PJ-AI-R31) <leonel.rozo@de.bosch.com>
2021-06-03 20:36:52 +0200

    Added script to train grid graph.

---
+++ train-grid.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Copyright (c) 2018-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+python3 embed.py \
+       -dim 2 \
+       -lr 0.3 \
+       -epochs 300 \
+       -negs 5 \
+       -burnin 20 \
+       -ndproc 4 \
+       -model distance \
+       -manifold poincare \
+       -dset wordnet/grid_nodes.csv \
+       -checkpoint grid_nodes.pth \
+       -batchsize 10 \
+       -eval_each 1 \
+       -fresh \
+       -sparse \
+       -train_threads 2
