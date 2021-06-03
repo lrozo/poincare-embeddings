507ced7a6b2a1c93bd671363cec230f5130da7ec
Rozo Leonel (CR/PJ-AI-R31) <leonel.rozo@de.bosch.com>
2021-06-03 20:35:41 +0200

    Added visualization.

--- reconstruction.py
+++ reconstruction.py
@@ -11,17 +11,18 @@ import numpy as np
 import torch
 import os
 import timeit
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from hype import MANIFOLDS, MODELS
 
 np.random.seed(42)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('file', help='Path to checkpoint')
+parser.add_argument('-file', help='Path to checkpoint', default='grid_nodes.pth.best')
 parser.add_argument('-workers', default=1, type=int, help='Number of workers')
 parser.add_argument('-sample', type=int, help='Sample size')
 parser.add_argument('-quiet', action='store_true', default=False)
 args = parser.parse_args()
-
 
 chkpnt = torch.load(args.file)
 dset = chkpnt['conf']['dset']
@@ -30,6 +31,21 @@ if not os.path.exists(dset):
 
 format = 'hdf5' if dset.endswith('.h5') else 'csv'
 dset = load_adjacency_matrix(dset, format, objects=chkpnt['objects'])
+
+colors = cm.rainbow(np.linspace(0, 1, 25))
+#circle = visualization.PoincareDisk(point_type='ball')
+plt.figure(figsize=(10, 10))
+ax = plt.subplot(111)
+# circle.add_points(gs.array([[0, 0]]))
+#circle.set_ax(ax)
+#circle.draw(ax=ax)
+embeddings = chkpnt['embeddings'].numpy()
+for i_embedding, embedding in enumerate(embeddings):
+    plt.scatter(embedding[0], embedding[1], alpha=1.0, color=colors[i_embedding],
+                label=i_embedding)
+    plt.annotate(chkpnt['objects'][i_embedding], (embedding[0], embedding[1]))
+# plt.legend()
+plt.show()
 
 sample_size = args.sample or len(dset['ids'])
 sample = np.random.choice(len(dset['ids']), size=sample_size, replace=False)
