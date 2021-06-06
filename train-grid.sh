c95a75a94ca2d1e80a892f5c81eec8ed9d216f47
Rozo Leonel (CR/PJ-AI-R31) <leonel.rozo@de.bosch.com>
2021-06-06 20:48:19 +0200

    Updated script params and saved grid CSV files.

--- train-grid.sh
+++ train-grid.sh
@@ -7,15 +7,15 @@
 
 python3 embed.py \
        -dim 2 \
-       -lr 0.3 \
-       -epochs 300 \
+       -lr 0.2 \
+       -epochs 500 \
        -negs 5 \
        -burnin 20 \
        -ndproc 4 \
        -model distance \
        -manifold poincare \
-       -dset robot_planning/grid_nodes.csv \
-       -checkpoint grid_nodes.pth \
+       -dset robot_planning/grid_nodes7.csv \
+       -checkpoint grid_nodes7.pth \
        -batchsize 10 \
        -eval_each 1 \
        -fresh \
